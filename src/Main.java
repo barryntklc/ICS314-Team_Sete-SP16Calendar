faa7755550e3a4ff196e7c69b4aa143df04a66f4
Barryn Chun <barryn.chun@gmail.com>
2016-02-22 07:27:00 -1000

    Initial Commit
    Added a starting class and main method.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main
+ * 
+ * Main class for Calendar_IO
+ * 
+ * @author Wing Yiu Ng
+ * @author Bobby White
+ * @author Barryn Chun
+ */
+public class Main {
+	public static void main (String [] args) {
+	
+		System.out.println("test");
+		
+	}
+}
