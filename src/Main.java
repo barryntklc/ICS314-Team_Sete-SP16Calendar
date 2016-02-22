7df04117704f80d7c9dc5cae95a93842614c9335
Chunmeista <barryn.chun@gmail.com>
2016-02-22 08:59:35 -1000

    commit02

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,22 @@
  * @author Barryn Chun
  */
 public class Main {
+	
+	/**
+	 * main
+	 * 
+	 * The main class.
+	 * 
+	 * @param args
+	 */
 	public static void main (String [] args) {
 	
 		System.out.println("test");
 		
 	}
+	
+	/**
+	 * TODO write method that prints calendar data to a file here.
+	 */
+	
 }
