7c040b7d666395a0657c0bc572dfe475866d8476
Chunmeista <barryn.chun@gmail.com>
2016-02-23 02:24:39 -1000

    commit03
    Added the method toFile which handles file operations. Successfully
    printed a .ics file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * Main
  * 
@@ -8,7 +12,7 @@
  * @author Barryn Chun
  */
 public class Main {
-	
+
 	/**
 	 * main
 	 * 
@@ -16,14 +20,67 @@ public class Main {
 	 * 
 	 * @param args
 	 */
-	public static void main (String [] args) {
-	
+	public static void main(String[] args) {
+
 		System.out.println("test");
+
+		String calendar_name = "TESTIMPORT";
+		String description = "This is a test event";
+		String location = "Hamilton Library";
+		String summary = "Study for exam";
 		
+		//TODO Replace with editable values
+		String date_start = "20160201T230000Z";
+		String date_end = "20160101T000001Z";
+		String date_stamp = "20160223T032827Z";
+		String date_created = "20160223T032617Z";
+		String date_modified = "20160223T032739Z";
+		
+		String output = "";
+
+		output = output + "BEGIN:VCALENDAR" + "\n";
+		output = output + "PRODID:-//Google Inc//Google Calendar 70.9054//EN" + "\n";
+		output = output + "VERSION:2.0" + "\n";
+		output = output + "CALSCALE:GREGORIAN" + "\n";
+		output = output + "METHOD:PUBLISH" + "\n";
+		output = output + "X-WR-CALNAME:" + calendar_name + "\n";
+		output = output + "X-WR-TIMEZONE:Pacific/Honolulu" + "\n"; //timezone
+		output = output + "X-WR-CALDESC:" + "\n";
+		output = output + "BEGIN:VEVENT" + "\n";
+		
+		output = output + "DTSTART:" + date_start + "\n"; //TODO make custom timestamp
+		output = output + "DTEND:" + date_end + "\n"; //TODO make custom timestamp
+		output = output + "DTSTAMP:" + date_stamp + "\n"; //TODO make custom timestamp
+		output = output + "UID:rhh4l2hdp1snqc7ego4lueh18c@google.com" + "\n";
+		output = output + "CREATED:" + date_created + "\n"; //TODO make custom timestamp
+		output = output + "DESCRIPTION:" + description + "\n";
+		output = output + "LAST-MODIFIED:" + date_modified + "\n"; //TODO make custom timestamp
+		output = output + "LOCATION:" + location + "\n";
+		output = output + "SEQUENCE:1" + "\n";
+		output = output + "STATUS:CONFIRMED" + "\n";
+		output = output + "SUMMARY:" + summary + "\n";
+		output = output + "TRANSP:TRANSPARENT" + "\n";
+		
+		output = output + "END:VEVENT" + "\n";
+		
+		output = output + "END:VCALENDAR" + "\n";
+
+		toFile(output);
 	}
-	
+
 	/**
-	 * TODO write method that prints calendar data to a file here.
+	 * Writes given text that prints calendar data to a file here.
 	 */
-	
+	private static void toFile(String input) {
+		System.out.println("DEBUG: toFile called!");
+		System.out.println(input);
+		
+		try {
+			PrintWriter output = new PrintWriter("newfile.ics");
+			output.write(input);
+			output.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: The file was not found.");
+		}
+	}
 }
