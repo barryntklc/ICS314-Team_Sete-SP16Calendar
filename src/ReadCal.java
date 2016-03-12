6f40fa0bd2618f3dfe9087b33e04c356afae2495
Chunmeista <barryn.chun@gmail.com>
2016-03-11 16:05:05 -1000

    commit16

---
+++ src/ReadCal.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * ReadCal
+ * 
+ * Methods used to read a .ics calendar can be written here.
+ * 
+ * @author Chunmeista
+ */
+
+public class ReadCal {
+
+	ArrayList<ArrayList<String>> fileContents = new ArrayList<ArrayList<String>>();
+	
+	public ReadCal(String filename) {
+		//given a filename, read its contents into the ArrayList of ArrayLists
+		//each ArrayList is a PARAMETER : VALUE pair, split by colon as the delimiter
+		
+	}
+	
+}
---
+++ src/WriteCal.java
@@ -0,0 +1,12 @@
+
+/**
+ * WriteCal
+ * 
+ * Methods used to write the file to the .ics file can be copied here.
+ * 
+ * @author Chunmeista
+ */
+
+public class WriteCal {
+
+}
